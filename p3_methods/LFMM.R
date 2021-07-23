b5048812d7df3bce425f0d4e92ed4a8a1aa6dc22
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-07-23 18:13:53 -0400

    add elbow function

--- p3_methods/LFMM.R
+++ p3_methods/LFMM.R
@@ -2,7 +2,6 @@ library("here") #paths
 #to install LFMM:
 #devtools::install_github("bcm-uga/lfmm")
 library("lfmm") #LFMM
-library("bigpca")
 library("vcfR")
 library("foreach")
 library("doParallel")
@@ -13,6 +12,77 @@ source("general_functions.R")
 ##########
 #  LFMM  #
 ##########
+# quickly choose an elbow for a PC. 
+# at variance below 5% per component, choose the largest % drop
+# designed for variance percentages, but will also work given a full set of Evalues
+#' Quickly estimate the 'elbow' of a scree plot (PCA)
+#' 
+#' This function uses a rough algorithm to estimate a sensible 'elbow' to
+#' choose for a PCA scree plot of eigenvalues. The function looks at an initial arbitrarily 'low'
+#' level of variance and looks for the first eigenvalue lower than this. If the very first eigenvalue
+#' is actually lower than this (i.e, when the PCs are not very explanatory) then this 'low' value is
+#' iteratively halved until this is no longer the case. After starting below this arbitrary threshold
+#' the drop in variance explained by each pair of consecutive PCs is standardized by dividing over the 
+#' larger of the pair. The largest percentage drop in the series below 'low' % is selected as the 'elbow'.
+#' @param varpc numeric, vector of eigenvalues, or 'percentage of variance' explained datapoints for
+#'  each principle component. If only using a partial set of components, should first pass to 
+#'  estimate.eig.vpcs() to estimate any missing eigenvalues.
+#' @param low numeric, between zero and one, the threshold to define that a principle component
+#'  does not explain much 'of the variance'.
+#' @param max.pc maximum percentage of the variance to capture before the elbow (cumulative sum to PC 'n')
+#' @return The number of last principle component to keep, prior to the determined elbow cutoff
+#' @export
+#' @seealso \code{\link{estimate.eig.vpcs}}
+#' @author Nicholas Cooper 
+#' @examples
+#' # correlated data
+#' mat <- sim.cor(100,50)
+#' result <- princomp(mat)
+#' eig <- result$sdev^2
+#' elb.a <- quick.elbow(eig)
+#' pca.scree.plot(eig,elbow=elb.a,M=mat) 
+#' elb.b <- quick.elbow(eig,low=.05) # decrease 'low' to select more components
+#' pca.scree.plot(eig,elbow=elb.b,M=mat) 
+#' # random (largely independent) data, usually higher elbow #
+#' mat2 <- generate.test.matrix(5,3)
+#' result2 <- princomp(mat2)
+#' eig2 <- result2$sdev^2
+#' elb2 <- quick.elbow(result2$sdev^2)
+#' pca.scree.plot(eig2,elbow=elb2,M=mat2)
+quick.elbow <- function(varpc,low=.08,max.pc=.9) {
+  ee <- varpc/sum(varpc) # ensure sums to 1
+  #print(round(log(ee),3))
+  while(low>=max(ee)) { low <- low/2 } # when no big components, then adjust 'low'
+  lowie <- (ee<low) ; highie <- ee>low/8
+  low.ones <- which(lowie & highie)
+  others <- length(which(!lowie))
+  if(length(low.ones)>0) {
+    if(length(low.ones)==1) {
+      elbow <- low.ones 
+    } else {
+      set <- ee[low.ones]
+      pc.drops <- abs(diff(set))/(set[1:(length(set)-1)])
+      infz <- is.infinite(pc.drops)
+      #print(pc.drops)
+      elbow <- which(pc.drops==max(pc.drops[!infz],na.rm=T))[1]+others
+    }
+  } else { 
+    # if somehow there are no small eigenvalues, just choose the elbow as the second last
+    cat("no eigenvalues were significantly smaller than the previous\n")
+    elbow <- length(ee) 
+  }
+  if(tail(cumsum(ee[1:elbow]),1)>max.pc) {
+    elbow <- which(cumsum(ee)>max.pc)[1]-1
+  }
+  if(elbow<1) {
+    warning("elbow calculation failed, return zero")
+    return(0)
+  }
+  names(elbow) <- NULL
+  return(elbow)
+}
+
+
 run_lfmm_full <- function(gen, gsd_df, loci_df){
   #get adaptive loci
   loci_trait1 <- loci_df$trait1 + 1 #add one to convert from python to R indexing
