94b6c25a1deeea5a29f0a47d6446db4c3a855dda
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-08-01 17:26:13 -0400

    Update test_data.R

--- p3_methods/test_data.R
+++ p3_methods/test_data.R
@@ -4,6 +4,6 @@ library("here")
 library("vcfR")
 library("lfmm")
 
-gsd_df <- get_gsd("/Users/Anusha/Documents/GitHub/LandGenSamp/p3_methods/data/GNX_mod-K3_phi50_m25_seed1_H5_r30/it--1/spp-spp_0/mod-K3_phi50_m25_seed1_H5_r30_it-1_t-1000_spp-spp_0.csv")
-gen <- get_gen("/Users/Anusha/Documents/GitHub/LandGenSamp/p3_methods/data/GNX_mod-K3_phi50_m25_seed1_H5_r30/it--1/spp-spp_0/mod-K3_phi50_m25_seed1_H5_r30_it-1_t-1000_spp-spp_0.vcf")
-
+gsd_df <- get_gsd("/Users/Anusha/Documents/GitHub/LandGenSamp/p3_methods/data/mod-K2_phi50_m25_seed1_H50_r60_it--1_t-1000_spp-spp_0.csv")
+gen <- get_gen("/Users/Anusha/Documents/GitHub/LandGenSamp/p3_methods/data/mod-K2_phi50_m25_seed1_H50_r60_it--1_t-1000_spp-spp_0.vcf")
+loci_df <- read.csv("/Users/Anusha/Documents/GitHub/LandGenSamp/p3_methods/data/nnloci_K2_phi50_m25_seed1_H50_r60.csv")
