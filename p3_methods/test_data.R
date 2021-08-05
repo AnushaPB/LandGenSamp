e6355110162b8df1e1b46e72af1c0a393d3c117f
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-08-05 00:21:20 -0400

    updates to methods

--- p3_methods/test_data.R
+++ p3_methods/test_data.R
@@ -12,3 +12,40 @@ loci_df <- read.csv("/Users/Anusha/Documents/GitHub/LandGenSamp/p3_methods/data/
 subIDs <- as.character(samples)
 gen <- gen[subIDs,]
 gsd_df <- gsd_df[subIDs,]
+
+
+#make grid for kriging
+x.range <- c(0,40)
+y.range <- c(0,40)
+krig_df.grd <- expand.grid(x=seq(from=x.range[1], to=x.range[2], len=40),  
+                           y=seq(from=y.range[1], to=y.range[2], len=40))
+coordinates(krig_df.grd) <- ~x+y
+gridded(krig_df.grd) <- TRUE
+
+
+krig_df = data.frame(x = gsd_df$x,
+                     y = gsd_df$y, 
+                     prop = gsd_df$env1)
+
+coordinates(krig_df)=~x+y
+
+krig_res <- autoKrige(prop ~ 1, krig_df, krig_df.grd)
+krig_raster1 <- raster(krig_res$krige_output)
+print(extent(krig_raster1))
+extent(krig_raster1) <- c(0,40,0,40)
+
+
+krig_df = data.frame(x = gsd_df$x,
+                     y = gsd_df$y, 
+                     prop = gsd_df$env2)
+
+coordinates(krig_df)=~x+y
+
+krig_res <- autoKrige(prop ~ 1, krig_df, krig_df.grd)
+krig_raster2 <- raster(krig_res$krige_output)
+print(extent(krig_raster2))
+extent(krig_raster2) <- c(0,40,0,40)
+
+par(mfrow=c(1,2))
+plot(krig_raster1, col=viridis(100))
+plot(krig_raster2, col=viridis(100))
