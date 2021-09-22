001d62d2b753a092e83d90e194f7633056183939
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-09-22 12:35:15 -0400

    update samp

---
+++ p2_sampling/sitesampling/site_functions.R
@@ -0,0 +1,29 @@
+
+SiteSample <- function(sample_sites, coords, npts, buffer_size = 300000){
+  #sample_sites - coordinates of sampling sites
+  #coords - coordinates of all individuals in data set
+  #npts - number of points to sample from each site
+  #buffer - buffer around site from which to draw samples randomly
+  site_samples <- data.frame()
+  for(i in 1:nrow(sample_sites)){
+    #create buffer around sites from which to sample points
+    site_buffers <- buffer(sample_sites[i,], 200000)
+    #subset out only coordinates falling within site buffers
+    buffer_samples <- coords[site_buffers,]
+    #convert from SPDF to df
+    buffer_samples_df <- data.frame(buffer_samples)
+    #randomly sample samples from coordinates
+    buffer_samples_df <- buffer_samples_df[sample(nrow(buffer_samples_df), npts),]
+    
+    #THINK ABOUT THIS CODE
+    #calculate the mean x coord (psuedo-site)
+    #buffer_samples_df$xsite <- mean(buffer_samples_df$x)
+    #calculate the mean y coord (psuedo-site)
+    #buffer_samples_df$ysite <- mean(buffer_samples_df$y)
+    
+    #bind samples
+    site_samples <- rbind(site_samples, buffer_samples_df)
+    site_samples$xsite
+  }
+}
+  
--- p2_sampling/sitesampling/site_random_sampling.R
+++ p2_sampling/sitesampling/site_random_sampling.R
@@ -1,4 +1,5 @@
 source("general_functions.R")
+source("sitesampling/site_functions.R")
 library("here")
 library("foreach")
 library("doParallel")
@@ -9,9 +10,15 @@ cores <- detectCores()
 cl <- makeCluster(cores[1]-3) #not to overload your computer
 registerDoParallel(cl)
 
-for(n in npts){
+nsite <- c(9, 18, 36)
+npts <- 10
+
+
+for(n in nsite){
   samples <- foreach(i=1:nrow(params), .combine=rbind) %dopar% {
     library("here")
+    library("raster")
+    library("sp")
     
     #create file path
     gsd_filepath <- create_filepath(i, params = params, "gsd")
@@ -25,9 +32,27 @@ for(n in npts){
     
     #run sampling
     if(skip_to_next == FALSE){
+      #get data
       gsd_df <- get_gsd(gsd_filepath)
-      #take random sample
-      samples <- gsd_df[sample(1:nrow(gsd_df), n), "idx"]
+      coords <- gsd_df[,c("idx", "x","y")]
+      coordinates(coords) <- ~x+y
+      
+      #randomly select points to act as sites
+      #buffer away from edges
+      coords_buffer <- crop(coords, extent(5,35,5,35))
+      #plot(coords)
+      #points(coords_buffer, col="red")
+      sample_sites <- coords_buffer[sample(1:length(coords_buffer), n),]
+
+      site_samples <- SiteSample(sample_sites, coords, npts, buffer_size = 300000)
+      
+      #plot (for debugging)
+      plot(sample_sites, xlim = c(0,40), ylim = c(0,40))
+      points(gsd_df[,c("x","y")], col = "gray")
+      points(site_samples[,c("x","y")], col = "red")
+      #points(site_samples[,c("xsite","ysite")], col = "blue", pch = 19)
+      
+      samples <- site_samples$idx 
     }
     
     #return vector of sample IDs
@@ -40,12 +65,9 @@ for(n in npts){
   samp_out <- params
   for(i in 1:ncol(samples)){samp_out <- cbind.data.frame(samp_out, samples[,i])}
   colnames(samp_out) <- c(colnames(params), colnames(samples))
-  write.csv(samp_out, paste0("outputs/samples_rand",n,".csv"), row.names = FALSE)
+  write.csv(samp_out, paste0("outputs/site_samples_rand",n,".csv"), row.names = FALSE)
 }
-
 
 #stop cluster
 stopCluster(cl)
-  
-
 
