983c93359432aed3688764fcb958c51bece3531c
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-10-27 16:33:37 -0400

    update random sampling to match changes in grid sampling

--- p2_sampling/sitesampling/site_random_sampling.R
+++ p2_sampling/sitesampling/site_random_sampling.R
@@ -10,7 +10,7 @@ cores <- detectCores()
 cl <- makeCluster(cores[1]-3) #not to overload your computer
 registerDoParallel(cl)
 
-for(n in nsite){
+for(n in nsites){
   samples <- foreach(i=1:nrow(params), .combine=rbind) %dopar% {
     library("here")
     library("raster")
@@ -33,16 +33,17 @@ for(n in nsite){
       coords <- gsd_df[,c("idx", "x","y")]
       coordinates(coords) <- ~x+y
       
-      #randomly select points to act as sites
+      
       #buffer away from edges
       coords_buffer <- crop(coords, extent(5,35,5,35))
       #plot(coords)
       #points(coords_buffer, col="red")
+      #randomly select points to act as sites
       sample_sites <- coords_buffer[sample(1:length(coords_buffer), n),]
 
       #sample from around sites based on a buffer
       #400000 chosen arbitrarily, 300000 was too small/not enough points in buffer for smaller sample sizes
-      site_samples <- SiteSample(sample_sites, coords, npts, buffer_size = 400000)
+      site_samples <- SiteSample(sample_sites, coords, npts = 10, buffer_size = 400000)
       
       #plot (for debugging)
       plot(sample_sites, xlim = c(0,40), ylim = c(0,40))
