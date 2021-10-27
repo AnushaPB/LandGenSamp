3935081e0604aa7ea7e7e359809ace295056f860
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-10-27 02:44:42 -0400

    change sampling buffer

--- p2_sampling/sitesampling/site_random_sampling.R
+++ p2_sampling/sitesampling/site_random_sampling.R
@@ -41,7 +41,8 @@ for(n in nsite){
       sample_sites <- coords_buffer[sample(1:length(coords_buffer), n),]
 
       #sample from around sites based on a buffer
-      site_samples <- SiteSample(sample_sites, coords, npts, buffer_size = 300000)
+      #400000 chosen arbitrarily, 300000 was too small/not enough points in buffer for smaller sample sizes
+      site_samples <- SiteSample(sample_sites, coords, npts, buffer_size = 400000)
       
       #plot (for debugging)
       plot(sample_sites, xlim = c(0,40), ylim = c(0,40))
