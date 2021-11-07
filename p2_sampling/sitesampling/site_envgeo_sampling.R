05fefe8fa1294260afc72d792d15b5445ceef3e2
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2021-11-07 15:39:24 -0500

    Modify envgeo sampling for site based sampling

--- p2_sampling/sitesampling/site_envgeo_sampling.R
+++ p2_sampling/sitesampling/site_envgeo_sampling.R
@@ -1,4 +1,6 @@
+
 source("general_functions.R")
+source("sitesampling/site_functions.R")
 library("here")
 library("foreach")
 library("doParallel")
@@ -6,18 +8,24 @@ library("vegan")
 
 set.seed(42)
 
-envgeo_samp <- function(gsd_df, nsite, Nreps = 1000){
+envgeo_samp <- function(gsd_df, nsite, Nreps = 1000, buffer = 5, ldim = 40){
   Nreps <- 1000
   sample.sets <- matrix(nrow=Nreps, ncol=nsite)
   results <- data.frame(env1.var=numeric(Nreps), env2.var=numeric(Nreps),
                         Mantel.r=numeric(Nreps), Mantel.p=numeric(Nreps),
                         mean.dist=numeric(Nreps))
+  #define buffer
+  buffmin <- buffer
+  buffmax <- ldim - buffer
+  #buffer coordinates away from edge
+  gsd_df <- gsd_df[gsd_df$x > buffmin & gsd_df$x < buffmax & gsd_df$y > buffmin & gsd_df$y < buffmax,]
   
   env.df <- gsd_df[,c("env1","env2")]
+  
   e.dist <-  as.matrix(dist(gsd_df[,c("env1","env2")], diag = TRUE, upper = TRUE)) 
   g.dist <- as.matrix(dist(gsd_df[,c("x","y")], diag = TRUE, upper = TRUE))
   for(i in 1:Nreps){
-    NN <- sample(1:nrow(gsd_df), npts, replace = FALSE)
+    NN <- sample(1:nrow(gsd_df), nsite, replace = FALSE)
     sample.sets[i,] <- NN
     
     env.sub <- env.df[NN,]
@@ -38,7 +46,6 @@ envgeo_samp <- function(gsd_df, nsite, Nreps = 1000){
   best_sample <- sample.sets[which.max(score),]
   sub_df <- gsd_df[best_sample,]
   
-  site_samples <- sub_df[,c]
   
   #save IDs to vector
   samples <- as.character(sub_df$idx)
@@ -52,12 +59,13 @@ cl <- makeCluster(cores[1]-3) #not to overload your computer
 registerDoParallel(cl)
 
 #sites 
-npts <- c(9, 16, 25)
+nsites <- c(9, 16, 25)
 
-for(n in npts){
+for(n in nsites){
   samples <- foreach(i=1:nrow(params), .combine=rbind) %dopar% {
     library("here")
     library("vegan")
+    library("raster")
     
     #create file path
     gsd_filepath <- create_filepath(i, params = params, "gsd")
@@ -72,7 +80,29 @@ for(n in npts){
     #run sampling
     if(skip_to_next == FALSE){
       gsd_df <- get_gsd(gsd_filepath)
-      samples <- envgeo_samp(gsd_df, npts = n, Nreps = 1000)
+      pts <- gsd_df[,c("idx","x","y")]
+      coords <- pts
+      coordinates(coords) <- ~x+y
+      
+      #grid sample sites
+      #note - buffer 5 from ldim so sites aren't sampled close to the edge
+      sample_sites <- envgeo_samp(gsd_df, nsite = n, Nreps = 1000, buffer = 5, ldim = 40)
+      #overwrite sample sites with coordinates for sample sites using indexes
+      sample_sites <- gsd_df[sample_sites, c("x","y")]
+      #convert to coordinates
+      coordinates(sample_sites) <- ~x+y
+      
+      #sample from around sites based on a buffer
+      #400000 chosen arbitrarily, 300000 was too small/not enough points in buffer for smaller sample sizes
+      site_samples <- SiteSample(sample_sites, coords, npts = 10, buffer_size = 400000)
+      
+      #plot (for debugging)
+      plot(sample_sites, xlim = c(0,40), ylim = c(0,40))
+      points(gsd_df[,c("x","y")], col = "gray")
+      points(site_samples[,c("x","y")], col = "red")
+      #points(site_samples[,c("xsite","ysite")], col = "blue", pch = 19)
+      
+      samples <- paste0(site_samples$idx, "_", site_samples$site)
     }
     
     #return vector of sample IDs
@@ -81,11 +111,24 @@ for(n in npts){
   }
   
   #bind sample IDs together and export (rows are parameter sets/columns are individual IDs)
-  colnames(samples) <- paste0("envgeo",1:ncol(samples))
+  colnames(samples) <- paste0("envgeo", 1:ncol(samples))
+  #sample IDs
+  sampleIDs <- gsub("\\_.*","",samples)
+  #site IDs
+  siteIDs <- gsub("^.*\\_","", samples)
+  
+  #create df of sample IDs
   samp_out <- params
-  for(i in 1:ncol(samples)){samp_out <- cbind.data.frame(samp_out, samples[,i])}
-  colnames(samp_out) <- c(colnames(params),colnames(samples))
-  write.csv(samp_out, paste0("outputs/samples_envgeo",n,".csv"), row.names = FALSE)
+  for(i in 1:ncol(samples)){samp_out <- cbind.data.frame(samp_out, sampleIDs[,i])}
+  colnames(samp_out) <- c(colnames(params), colnames(samples))
+  write.csv(samp_out, paste0("outputs/site_samples_envgeo",n,".csv"), row.names = FALSE)
+  
+  #create df of site IDs
+  site_out <- params
+  for(i in 1:ncol(samples)){site_out <- cbind.data.frame(site_out, siteIDs[,i])}
+  colnames(site_out) <- c(colnames(params), colnames(samples))
+  write.csv(site_out, paste0("outputs/site_ids_envgeo",n,".csv"), row.names = FALSE)
+  
 }
 
 
