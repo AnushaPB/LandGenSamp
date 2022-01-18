155226558c8ec85dea5d971c79d2932a66a02ea3
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-01-18 15:19:05 -0500

    Replace hard paths

--- p3_methods/sitesampling_functions.R
+++ p3_methods/sitesampling_functions.R
@@ -1,14 +1,11 @@
 
 
 #get list of sampling IDs that correspond with parameter set, sampling strategy, and number of samples
-get_samples <- function(param_set, params, sampstrat, nsamp){
+get_samples <- function(param_set, params, sampstrat, nsamp, datadir =  here(dirname(getwd()), "p2_gnxsims", "outputs")){
   #param_set - vector of one set of parameters (e.g. params[i,])
   #params - full set of parameters
   #sampstrat - sampling strategy (e.g. "rand", "grid", "trans", "envgeo")
   #nsamp - number of samples
-  
-  #directory of sample ID csvs (CHANGE)
-  datadir <- "/Users/Anusha/Documents/GitHub/LandGenSamp/p2_sampling/outputs/"
   
   subIDs <- read.csv(paste0(datadir, "samples_", sampstrat, nsamp, ".csv"))
     
@@ -34,14 +31,11 @@ get_samples <- function(param_set, params, sampstrat, nsamp){
 }
 
 #get list of sampling IDs that correspond with parameter set, sampling strategy, and number of samples
-get_samples <- function(param_set, params, sampstrat, nsamp){
+get_samples <- function(param_set, params, sampstrat, nsamp,  datadir =  here(dirname(getwd()), "p2_gnxsims", "outputs")){
   #param_set - vector of one set of parameters (e.g. params[i,])
   #params - full set of parameters
   #sampstrat - sampling strategy (e.g. "rand", "grid", "trans", "envgeo")
   #nsamp - number of samples
-  
-  #directory of sample ID csvs (CHANGE)
-  datadir <- "/Users/Anusha/Documents/GitHub/LandGenSamp/p2_sampling/outputs/"
   
   subIDs <- read.csv(paste0(datadir, "site_samples_", sampstrat, nsamp, ".csv"))
   
@@ -67,14 +61,11 @@ get_samples <- function(param_set, params, sampstrat, nsamp){
 }
 
 #get list of site IDs that correspond with parameter set, sampling strategy, and number of samples (and sample IDs)
-get_sites <- function(param_set, params, sampstrat, nsamp){
+get_sites <- function(param_set, params, sampstrat, nsamp,  datadir =  here(dirname(getwd()), "p2_gnxsims", "outputs")){
   #param_set - vector of one set of parameters (e.g. params[i,])
   #params - full set of parameters
   #sampstrat - sampling strategy (e.g. "rand", "grid", "trans", "envgeo")
   #nsamp - number of samples
-  
-  #directory of sample ID csvs (CHANGE)
-  datadir <- "/Users/Anusha/Documents/GitHub/LandGenSamp/p2_sampling/outputs/"
   
   subIDs <- read.csv(paste0(datadir, "site_ids_", sampstrat, nsamp, ".csv"))
   
