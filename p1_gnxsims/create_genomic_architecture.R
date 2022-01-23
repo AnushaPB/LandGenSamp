3822685b1d446d09b914a61cbe2cf9647057f431
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-01-23 17:51:12 -0500

    Create create_genomic_architecture.R

---
+++ p1_gnxsims/create_genomic_architecture.R
@@ -0,0 +1,27 @@
+#code to create a genomic architecture file
+library(here)
+#from gnx docs:
+#CSV file with loci as rows and ‘locus’, ‘p’, ‘dom’, ‘r’, ‘trait’, and ‘alpha’ as columns 
+#stipulating the locus numbers, starting allele frequencies, dominance values, inter-locus recombination rates, trait names, 
+#and effect sizes of all loci; values can be left blank if not applicable).
+
+#total number of loci
+nloci <- 100000
+
+#create data frame
+df <- data.frame(locus = 1:nloci,
+           p = 0.5,
+           dom = NA,
+           r = NA,
+           trait = NA,
+           alpha = NA)
+
+#specify which loci belong to which traits
+#in this case there are two traits, each with 4 loci
+#their location does not matter because there is no linkage
+df$trait[1:4] <- "trait_1"
+df$trait[5:8] <- "trait_2"
+
+#write out file
+path <- here(dirname(getwd()), "p1_gnxsims", "genomic_architecture.csv")
+write.csv(df, path, row.names = FALSE)
