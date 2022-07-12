c1b6269708b2a1d9ab869347576bb71eda7502d1
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-07-12 11:17:01 -0700

    try fc

--- p3_methods/LFMM_sitesampling.R
+++ p3_methods/LFMM_sitesampling.R
@@ -15,25 +15,17 @@ source("sitesampling_functions.R")
 
 ##########
 #  LFMM  #
-##########
-
+##########S
 
 run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
-  
-  #for readibility, just negates the in function
-  `%notin%` <- Negate(`%in%`)
   
   #get adaptive loci
   loci_trait1 <- loci_df$trait1 + 1 #add one to convert from python to R indexing
   loci_trait2 <- loci_df$trait2 + 1 #add one to convert from python to R indexing
-  adaptive_loci <- c(loci_trait1, loci_trait2)
-  neutral_loci <- c(1:nloci)[-adaptive_loci]
   
-  
-  #if K is not specified it is calculated based on a tracy widom test
+  #if K is not specified it is calculated
   if(is.null(K)){
-    K <- get_K(gen, k_selection = "tracy.widom")
-    if(K == 0){ K <- 1 }
+    K <- get_K(gen, k_selection = "find.clusters")
   }
   
   
@@ -52,16 +44,32 @@ run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
                   X = envmat, 
                   lfmm = lfmm_mod, 
                   calibrate = "gif")
-  #adjust pvalues
-  pvalues <- data.frame(env1=p.adjust(pv$calibrated.pvalue[,1], method="fdr"),
-                        env2=p.adjust(pv$calibrated.pvalue[,2], method="fdr"))
+  
+  # correct pvals and get confusion matrix stats
+  p05 <- purrr::map_dfr(c("none", "fdr", "holm"), calc_confusion, pv, loci_trait1, loci_trait2, alpha = 0.05)
+  p10 <- purrr::map_dfr(c("none", "fdr", "holm"), calc_confusion, pv, loci_trait1, loci_trait2, alpha = 0.10)
+  pdf <- rbind.data.frame(p05, p10)
+  df <- data.frame(K = K, pdf)
+  
+  return(df)
+}
+
+calc_confusion <- function(padj, pv, loci_trait1, loci_trait2, alpha = 0.05){
+  
+  #for readibility, just negates the in function
+  `%notin%` <- Negate(`%in%`)
+  
+  # adjust pvalues (or passs through if padj = "none")
+  pvalues <-  data.frame(env1 = p.adjust(pv$calibrated.pvalue[,1], method = padj),
+                         env2 = p.adjust(pv$calibrated.pvalue[,2], method = padj))
+  
   #env1 candidate loci
   #Identify LFMM cand loci (P)
-  lfmm_loci1 <- which(pvalues[,"env1"] < 0.05) 
+  lfmm_loci1 <- which(pvalues[,"env1"] < alpha) 
   #Identify negatives
-  lfmm_neg1 <- which(pvalues[,"env1"] >= 0.05 | is.na(pvalues[,"env1"]))
+  lfmm_neg1 <- which(pvalues[,"env1"] >= alpha | is.na(pvalues[,"env1"]))
   #check length makes sense
-  stopifnot(length(lfmm_loci1) + length(lfmm_neg1) == ncol(gen))
+  stopifnot(length(lfmm_loci1) + length(lfmm_neg1) == nrow(pvalues))
   
   #get confusion matrix values
   #True Positives
@@ -73,15 +81,15 @@ run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
   #False Negatives
   FN1 <- sum(lfmm_neg1 %in% loci_trait1)
   #check sum makes sense
-  stopifnot(sum(TP1, FP1, TN1, FN1) == ncol(gen))
+  stopifnot(sum(TP1, FP1, TN1, FN1) == nrow(pvalues))
   
   #env2 candidate loci
   #Identify LFMM cand loci
-  lfmm_loci2 <- which(pvalues[,"env2"] < 0.05) 
+  lfmm_loci2 <- which(pvalues[,"env2"] < alpha) 
   #Identify negatives
-  lfmm_neg2 <- which(pvalues[,"env2"] >= 0.05 | is.na(pvalues[,"env2"]))
+  lfmm_neg2 <- which(pvalues[,"env2"] >= alpha | is.na(pvalues[,"env2"]))
   #check length makes sense
-  stopifnot(length(lfmm_loci2) + length(lfmm_neg2) == ncol(gen))
+  stopifnot(length(lfmm_loci2) + length(lfmm_neg2) == nrow(pvalues))
   
   #True Positives
   TP2 <- sum(lfmm_loci2 %in% loci_trait2)
@@ -92,7 +100,7 @@ run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
   #False Negatives
   FN2 <- sum(lfmm_neg2 %in% loci_trait2)
   #check length makes sense
-  stopifnot(sum(TP2, FP2, TN2, FN2) == ncol(gen))
+  stopifnot(sum(TP2, FP2, TN2, FN2) == nrow(pvalues))
   
   #stats for all loci 
   lfmm_loci <- c(lfmm_loci1, lfmm_loci2)
@@ -102,7 +110,7 @@ run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
   TN <- TN1 + TN2
   FN <- FN1 + FN2
   #check sum makes sense
-  stopifnot(sum(TP, FP, TN, FN) == 2*ncol(gen))
+  stopifnot(sum(TP, FP, TN, FN) == 2*nrow(pvalues))
   
   #calc True Positive Rate (i.e. Sensitivity)
   TPRCOMBO <- TP/(TP + FN)
@@ -113,7 +121,16 @@ run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
   #calc False Positive Rate 
   FPRCOMBO <- FP/(FP + TN)
   
-  return(data.frame(K = K,
+  # Calculate empirical pvalues (I THINK - CHECK THIS)
+  null1 <- pvalues$env1[-loci_trait1]
+  emp1 <- sapply(pvalues$env1[loci_trait1], function(x){mean(x > null1, na.rm = TRUE)})
+  emp1_TPR <- sum(emp1 < alpha, na.rm  = TRUE)
+  null2 <- pvalues$env2[-loci_trait2]
+  emp2 <- sapply(pvalues$env2[loci_trait2], function(x){mean(x > null2, na.rm = TRUE)})
+  emp2_TPR <- sum(emp2 < alpha, na.rm  = TRUE)
+  
+  return(data.frame(padj = padj,
+                    alpha = alpha,
                     TPRCOMBO = TPRCOMBO, 
                     TNRCOMBO = TNRCOMBO,
                     FDRCOMBO = FDRCOMBO, 
@@ -122,17 +139,30 @@ run_lfmm <- function(gen, gsd_df, loci_df, K = NULL){
                     TOTALTP = TP, 
                     TOTALFP = FP, 
                     TOTALTN = TN,
-                    TOTALFN = FN))
+                    TOTALFN = FN,
+                    emp1_TPR = emp1_TPR,
+                    emp2_TPR = emp2_TPR,
+                    emp1_mean = mean(emp1, na.rm = TRUE),
+                    emp2_mean = mean(emp2, na.rm = TRUE)))
 }
 
 
 # function to determine K
-get_K <- function(gen, coords = NULL, k_selection = "tracy.widom", Kvals = Kvals, ...){
+get_K <- function(gen, coords = NULL, k_selection = "find.clusters", ...){
   
-  if(k_selection == "tracy.widom"){K <- get_K_tw(gen)}
+  if(k_selection == "tracy.widom") K <- get_K_tw(gen)
   
-  if(k_selection == "quick.elbow"){K <- get_K_elbow(gen)}
+  if(k_selection == "quick.elbow") K <- get_K_elbow(gen)
   
+  if(k_selection == "find.clusters") K <- get_K_fc(gen)
+  
+  return(K)
+}
+
+# Determine best K using find.clusters
+get_K_fc <- function(gen, max.n.clust = 9, perc.pca = 70){
+  fc <- adegenet::find.clusters(gen,  pca.select = "percVar", perc.pca = perc.pca, choose.n.clust = FALSE, criterion = "diffNgroup", max.n.clust = max.n.clust)
+  K <- max(as.numeric(fc$grp))
   return(K)
 }
 
@@ -145,7 +175,7 @@ get_K_elbow <- function(gen){
   eig <- pc$sdev^2
   # estimate number of latent factors using quick.elbow (see general functions for description of how this function works)
   # this is a crude way to determine the number of latent factors that is based on an arbitrary "low" value 
-  K <- quick.elbow(eig, low = 0.08, max.pc = 0.9)
+  K <- quick.elbow(eig, low = 0.08, max.pc = 0.7)
   
   par(pty = "s",mfrow = c(1,1))
   plot(eig, xlab = 'PC', ylab = "Variance explained")
@@ -279,5 +309,5 @@ res_lfmm <- foreach(i=1:nrow(params), .combine=rbind, .packages = c("here", "vcf
 #stop cluster
 stopCluster(cl)
 
-write.csv(res_lfmm, "outputs/lfmm_sitesampling_results_tw.csv", row.names = FALSE)
+write.csv(res_lfmm, "outputs/lfmm_sitesampling_results_fc.csv", row.names = FALSE)
 
