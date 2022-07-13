bd1f8e66c5d08acd51abda2eaf392d7142e8237a
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-07-13 10:26:21 -0700

    refactor MMRR code and clean up

--- p3_methods/MMRR_indsampling.R
+++ p3_methods/MMRR_indsampling.R
@@ -6,13 +6,13 @@ library(doParallel)
 
 #read in general functions and objects
 source("general_functions.R")
-source("sitesampling_functions.R")
 source("MMRR_functions.R")
 
 #register cores
 cores <- 20
-cl <- makeCluster(cores)
+cl <- makeCluster(cores) 
 registerDoParallel(cl)
+
 
 res_mmrr <- foreach(i=1:nrow(params), .combine=rbind, .packages = c("here", "vcfR", "adegenet", "stringr")) %dopar% {
 
@@ -40,41 +40,33 @@ res_mmrr <- foreach(i=1:nrow(params), .combine=rbind, .packages = c("here", "vcf
     gsd_df <- get_data(i, params = params, "gsd")
     
     #subsample full data randomly
-    s <- sample(nrow(gsd_df), 2000, replace = FALSE)
+    s <- sample(nrow(gsd_df), 1000, replace = FALSE)
     gen_2k <- gen[s,]
     gsd_df_2k <- gsd_df[s,]
     
     #run model on full data set
     full_result <- run_mmrr(gen_2k, gsd_df_2k)
     fullratio <- (full_result$env1_coeff + full_result$env2_coeff)/full_result$geo_coeff
-    result <- data.frame(params[i,], sampstrat = "full", nsamp = nrow(gsd_df_2k), full_result, 
+    result <- data.frame(params[i,], 
+                         sampstrat = "full", 
+                         nsamp = nrow(gsd_df_2k), 
+                         full_result, 
                          ratio = fullratio, 
                          env1_err = NA, env2_err = NA, geo_err = NA, ratio_err = NA)
     
     #write full datafile (temp)
-    #csv_file <- paste0("outputs/MMRR/MMRR_sitesampling_results_",paramset,".csv")
+    #csv_file <- paste0("outputs/MMRR/MMRR_results_",paramset,".csv")
     #write.csv(result, csv_file, row.names = FALSE)
     
-    for(nsite in nsites){
+    for(nsamp in npts){
       for(sampstrat in sampstrats){
         #subsample from data based on sampling strategy and number of samples
-        subIDs <- get_samples(params[i,], params, sampstrat, nsite)
+        subIDs <- get_samples(params[i,], params, sampstrat, nsamp)
         subgen <- gen[subIDs,]
         subgsd_df <- gsd_df[subIDs,]
         
-        #get sites
-        siteIDs <- get_sites(params[i,], params, sampstrat, nsite)
-        #confirm that number of sites matches number of sample IDs
-        stopifnot(length(subIDs) == length(siteIDs))
-        #calculate allele frequency by site (average)
-        sitegen <- data.frame(aggregate(subgen, list(siteIDs), FUN=mean)[,-1])
-        #calculate env values by site
-        sitegsd_df <- data.frame(aggregate(subgsd_df, list(siteIDs), FUN=mean)[,-1]) 
-        #confirm that number of values matches number of sites
-        stopifnot(nsite == nrow(sitegsd_df) & nsite == nrow(sitegen))
-        
         #run analysis using subsample
-        sub_result <- run_mmrr(sitegen, sitegsd_df)
+        sub_result <- run_mmrr(subgen, subgsd_df)
         
         #calculate err
         env1_err <- err_coeff(full_result$env1_coeff, sub_result$env1_coeff)
@@ -85,7 +77,10 @@ res_mmrr <- foreach(i=1:nrow(params), .combine=rbind, .packages = c("here", "vcf
         ratio_err <- err_coeff(fullratio, subratio)
         
         #save and format new result
-        sub_result <- data.frame(params[i,], sampstrat = sampstrat, nsamp = nsite, sub_result, 
+        sub_result <- data.frame(params[i,], 
+                                 sampstrat = sampstrat, 
+                                 nsamp = nsamp, 
+                                 sub_result, 
                                  ratio = subratio,
                                  env1_err = env1_err, env2_err = env2_err, geo_err = geo_err,
                                  ratio_err = ratio_err)
@@ -109,4 +104,4 @@ res_mmrr <- foreach(i=1:nrow(params), .combine=rbind, .packages = c("here", "vcf
 #stop cluster
 stopCluster(cl)
 
-write.csv(res_mmrr, "outputs/mmrr_sitesampling_results.csv", row.names = FALSE)
+write.csv(res_mmrr, "outputs/mmrr_indsampling_results.csv", row.names = FALSE)
