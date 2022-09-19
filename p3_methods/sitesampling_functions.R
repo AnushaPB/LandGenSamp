8973345ccf9340f1198d0026404cfb5d5ede7b57
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2022-09-18 23:55:55 -0700

    remove extra sample function

--- p3_methods/sitesampling_functions.R
+++ p3_methods/sitesampling_functions.R
@@ -1,37 +1,6 @@
 
 
 #get list of sampling IDs that correspond with parameter set, sampling strategy, and number of samples
-get_samples <- function(param_set, params, sampstrat, nsamp, datadir =  here(dirname(getwd()), "p2_sampling", "outputs")){
-  #param_set - vector of one set of parameters (e.g. params[i,])
-  #params - full set of parameters
-  #sampstrat - sampling strategy (e.g. "rand", "grid", "trans", "envgeo")
-  #nsamp - number of samples
-  
-  subIDs_all <- read.csv(paste0(datadir, "/samples_", sampstrat, nsamp, ".csv"))
-    
-  subIDs <- subIDs_all[subIDs_all$K == param_set$K 
-                   & subIDs_all$phi == param_set$phi
-                   & subIDs_all$m == param_set$m 
-                   & subIDs_all$seed == param_set$seed
-                   & subIDs_all$H == param_set$H
-                   & subIDs_all$r == param_set$r
-                   & subIDs_all$it == param_set$it,]
-  
-  #confirm there is only one set of IDs being used
-  stopifnot(nrow(subIDs) != 0)
-  stopifnot(nrow(subIDs) == 1)
-  
-  #remove parameter columns and convert to vector of IDs
-  subIDs <- subIDs[,!names(subIDs) %in% colnames(params)]
-  subIDs <- unlist(subIDs)
-  
-  #confirm that final set of IDs is a vector
-  stopifnot(is.vector(subIDs))
-  
-  return(as.character(subIDs))
-}
-
-#get list of sampling IDs that correspond with parameter set, sampling strategy, and number of samples
 get_samples <- function(param_set, params, sampstrat, nsamp,  datadir =  here(dirname(getwd()), "p2_sampling", "outputs")){
   #param_set - vector of one set of parameters (e.g. params[i,])
   #params - full set of parameters
