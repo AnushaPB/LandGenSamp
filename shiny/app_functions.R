d02d2097fdfd1aa2a020765330eb69847becf477
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-05-20 13:06:09 -0700

    Update app_functions.R

--- shiny/app_functions.R
+++ shiny/app_functions.R
@@ -1,11 +1,6 @@
-get_data <- function(method, sampling) {
+get_app_data <- function(method, sampling) {
   df <- read_csv(here("shiny", "app_data", paste0(method, "_", sampling, ".csv")))
   # convert parameters to factors
   df <- var_to_fact(df) 
   return(df)
-}
-
-
-filter_data <- function(df){
-  
 }
