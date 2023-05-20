7f6255acf6fe8df2b263c708e514abe57fbcec5c
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-05-20 07:08:53 -0700

    Create app_functions.R

---
+++ shiny/app_functions.R
@@ -0,0 +1,11 @@
+get_data <- function(method, sampling) {
+  df <- read_csv(here("shiny", "app_data", paste0(method, "_", sampling, ".csv")))
+  # convert parameters to factors
+  df <- var_to_fact(df) 
+  return(df)
+}
+
+
+filter_data <- function(df){
+  
+}
