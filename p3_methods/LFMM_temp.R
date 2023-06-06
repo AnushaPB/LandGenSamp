4c05b6b641f88e243e2a3a3c5ef3d582a2672d4c
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-06-05 20:55:07 -0700

    Update LFMM_temp.R

--- p3_methods/LFMM_temp.R
+++ p3_methods/LFMM_temp.R
@@ -7,6 +7,7 @@ library(vcfR)
 library(lfmm)
 library(gdm)
 library(vegan)
+library(purrr)
 
 # Read in general functions and objects
 source(here("general_functions.R"))
@@ -15,6 +16,9 @@ source(here("p3_methods", "GEA_functions.R"))
 
 # run analysis for individual sampling
 method = "lfmm"
+ns = nsamps
+strats = sampstrats
+site = FALSE
 ind_results <- run_analysis(params, ns = nsamps, strats = sampstrats, method = method, full_result = NULL, site = FALSE, ncores = 20)
 path <- here("p3_methods", "outputs", paste0(method, "_indsampling_results.csv"))
 write.csv(ind_results, path, row.names = FALSE)
