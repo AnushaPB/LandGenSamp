458b51f60412cb18fb3fc1ad10f85aea53cfac99
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-06-06 11:05:06 -0700

    Update MMRR_temp.R

--- p3_methods/MMRR_temp.R
+++ p3_methods/MMRR_temp.R
@@ -20,8 +20,8 @@ ns = nsamps
 strats = sampstrats
 site = FALSE
 safe_save <- safely(save)
-full_result <- run_full(params[1:2, ], method = method, ncores = 20, n = 100) 
+full_result <- run_full(params, method = method, ncores = 20, n = 2000) 
 safe_save(full_result, file = "full_result.rda")
-ind_results <- run_analysis(params[1:2, ], ns = nsamps, strats = sampstrats, method = method, full_result = full_result, site = FALSE, ncores = 25)
+ind_results <- run_analysis(params, ns = nsamps, strats = sampstrats, method = method, full_result = full_result, site = FALSE, ncores = 25)
 path <- here("p3_methods", "outputs", paste0(method, "_indsampling_results.csv"))
 write.csv(ind_results, path, row.names = FALSE)
