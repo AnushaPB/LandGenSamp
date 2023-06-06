2a9588cb3e075273583f0bf3f415d35bd27f819a
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-06-06 10:59:56 -0700

    Create MMRR_temp.R

---
+++ p3_methods/MMRR_temp.R
@@ -0,0 +1,27 @@
+# Parallel processing libraries
+library(furrr)
+library(dplyr)
+library(tidyr)
+library(here)
+library(vcfR)
+library(lfmm)
+library(gdm)
+library(vegan)
+library(purrr)
+
+# Read in general functions and objects
+source(here("general_functions.R"))
+source(here("p3_methods", "general_run_functions.R"))
+source(here("p3_methods", "IBDIBE_functions.R"))
+
+# run analysis for individual sampling
+method = "mmrr"
+ns = nsamps
+strats = sampstrats
+site = FALSE
+safe_save <- safely(save)
+full_result <- run_full(params[1:2, ], method = method, ncores = 20, n = 100) 
+safe_save(full_result, file = "full_result.rda")
+ind_results <- run_analysis(params[1:2, ], ns = nsamps, strats = sampstrats, method = method, full_result = NULL, site = FALSE, ncores = 25)
+path <- here("p3_methods", "outputs", paste0(method, "_indsampling_results.csv"))
+write.csv(ind_results, path, row.names = FALSE)
