bad378c7289902d07674a39f0a3111590e2be455
AnushaPB <anusha.bishop@gmail.com>
2023-06-14 11:31:56 -0700

    rda

---
+++ p3_methods/RDA_temp.R
@@ -0,0 +1,24 @@
+# Parallel processing libraries
+library(furrr)
+library(dplyr)
+library(tidyr)
+library(here)
+library(vcfR)
+library(lfmm)
+library(gdm)
+library(vegan)
+library(purrr)
+
+# Read in general functions and objects
+source(here("general_functions.R"))
+source(here("p3_methods", "general_run_functions.R"))
+source(here("p3_methods", "GEA_functions.R"))
+
+# run analysis for individual sampling
+method = "rda"
+ns = nsamps
+strats = sampstrats
+site = FALSE
+ind_results <- run_analysis(params, ns = nsamps, strats = sampstrats, method = method, full_result = NULL, site = FALSE, ncores = 15)
+path <- here("p3_methods", "outputs", paste0(method, "_indsampling_results.csv"))
+write.csv(ind_results, path, row.names = FALSE)
