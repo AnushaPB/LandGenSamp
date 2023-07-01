17a6dafd18eb75bc07fef9dc6397b4a8fdaa1cf7
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2023-07-01 12:03:28 -0700

    Update LFMM_temp.R

--- p3_methods/LFMM_temp.R
+++ p3_methods/LFMM_temp.R
@@ -19,6 +19,6 @@ method = "lfmm"
 ns = nsamps
 strats = sampstrats
 site = FALSE
-ind_results <- run_analysis(params, ns = nsamps, strats = sampstrats, method = method, full_result = NULL, site = FALSE, ncores = 20)
+ind_results <- run_analysis(params, ns = nsamps, strats = sampstrats, method = method, full_result = NULL, site = site, ncores = 27)
 path <- here("p3_methods", "outputs", paste0(method, "_indsampling_results.csv"))
 write.csv(ind_results, path, row.names = FALSE)
