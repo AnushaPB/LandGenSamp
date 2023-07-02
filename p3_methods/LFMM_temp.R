73171382729d84a70dd6080e01e5aedf878e2152
AnushaPB <anusha.bishop@gmail.com>
2023-07-01 17:04:34 -0700

    u

--- p3_methods/LFMM_temp.R
+++ p3_methods/LFMM_temp.R
@@ -8,7 +8,8 @@ library(lfmm)
 library(gdm)
 library(vegan)
 library(purrr)
-
+library(doParallel)
+library(foreach)
 # Read in general functions and objects
 source(here("general_functions.R"))
 source(here("p3_methods", "general_run_functions.R"))
@@ -16,9 +17,8 @@ source(here("p3_methods", "GEA_functions.R"))
 
 # run analysis for individual sampling
 method = "lfmm"
-ns = nsites
-strats = sitestrats
-site = TRUE
-site_results <- run_analysis(params, ns = ns, strats = strats, method = method, full_result = NULL, site = site, ncores = 10)
-path <- here("p3_methods", "outputs", paste0(method, "_sitesampling_results.csv"))
-write.csv(site_results, path, row.names = FALSE)
+ns = nsamps
+strats = sampstrats
+site = FALSE
+ind_results <- run_analysis(params, ns = ns, strats = strats, method = method, full_result = NULL, site = site, ncores = 25)
+path <- here("p3_methods", "outputs", paste0(method, "_indsampling_results.csv"))write.csv(ind_results, path, row.names = FALSE)
