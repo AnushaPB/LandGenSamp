d4416f164112eada0699e97fc9390ec032d289f8
AnushaPB <anusha.bishop@berkeley.edu>
2023-10-27 22:00:45 -0700

    add conda_create

---
+++ p1_gnxsims/gnx/conda_create.sh
@@ -0,0 +1,15 @@
+# create conda env for geonomics
+conda create --name gnx python=3
+
+# for linux:
+source activate gnx
+
+# install packages
+conda install msprime
+pip3 install geonomics geopandas rasterio matplotlib scipy bitarray tskit scikit-learn statsmodels psutil nlmpy
+
+# export env
+conda env export > gnx.yml
+
+# to create based on yml
+#conda env create -f gnx.yml
