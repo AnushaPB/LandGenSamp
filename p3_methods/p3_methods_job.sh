65d0365d5aa8244ad97f501126cee5896ee3d56d
AnushaPB <anusha.bishop@berkeley.edu>
2024-01-27 02:52:08 -0800

    add zipped outputs

--- p3_methods/p3_methods_job.sh
+++ p3_methods/p3_methods_job.sh
@@ -10,4 +10,8 @@ Rscript phenotype_mismatch.R
 # Run landscape genomic methods
 Rscript run_methods.R
 
+# Compress outputs for git
+zip -r outputs.zip outputs
+# to decompress: unzip outputs
+
 cd ..
