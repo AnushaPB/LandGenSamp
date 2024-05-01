ad8e7b325eb7f7210f437b89f18de28bcec48cb1
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-04-30 22:28:48 -0700

    reorganize and rename files

--- p1_gnxsims/MNLM/create_MNLM.R
+++ p1_gnxsims/MNLM/create_MNLM.R
@@ -1,41 +1,10 @@
-
 library(raster)
 library(here)
 #install.packages("devtools")
 #devtools::install_github("ropensci/NLMR")
 library(NLMR)
-#options(repos = c(
-#  jeffreyevans = 'https://jeffreyevans.r-universe.dev',
-#  CRAN = 'https://cloud.r-project.org'))
-# Download and install spatialEco in R
-# install.packages('spatialEco')
-library(spatialEco)
-library(viridis)
-library(terra)
-
-wdir <- here("p1_gnxsims", "MNLM")
+library(tidyverse)
 
 combos <- expand.grid(seed = c(1, 2, 3), H = c(0.05, 0.5), r = c(0.3, 0.6))
-
 mnlms <- pmap(combos, mnlm_create, .progress = TRUE)
-
 walk(mnlms, mnlm_write)
-
-mnlms <- pmap(combos, mnlm_read)
-
-stats <- map(mnlms, mnlm_stats) %>% bind_rows()
-
-mnlm_df <- 
-  map(mnlms, ~as.data.frame(.x, xy = TRUE, ID = FALSE)) %>%
-  map(~pivot_longer(.x, -c(x, y), names_to = "env")) %>%
-  bind_rows() %>%
-  left_join(stats) %>%
-  mutate(name = paste0(env, " | M = ", M, " | r = ", r))
-
-ggplot(mnlm_df) +
-  geom_raster(aes(x = x, y = y, fill = value)) +
-  facet_wrap(~name, ncol = 4) +
-  theme_void() +
-  scale_fill_viridis_c(option = "magma") +
-  theme(legend.position = "none")
-
