026f217b70794b313fc5c0480ff3ac78c1fe9b8a
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-08-06 12:57:42 -0700

    Update p4_analysis_job.sh

--- p4_analysis/p4_analysis_job.sh
+++ p4_analysis/p4_analysis_job.sh
@@ -1,9 +1,9 @@
 # knit File S4 and S5
 # note 1: this creates the figure PDFs which are placed in the "figures" directory
-Rscript -e "rmarkdown::render(here::here('p4_analysis', 'FileS4_GEA.Rmd')"
-Rscript -e "rmarkdown::render(here::here('p4_analysis', 'FileS5_IBDIBE.Rmd')"
+Rscript -e "rmarkdown::render(here::here('p4_analysis', 'FileS6_GEA.Rmd')"
+Rscript -e "rmarkdown::render(here::here('p4_analysis', 'FileS7_IBDIBE.Rmd')"
+Rscript -e "rmarkdown::render(here::here('p4_analysis', 'FileS5_simulation_summary_statistics.Rmd')"
 
 # note 2: supplementary plots/other data visualizations were created from running notebooks interactively
 # analysis_megatable.Rmd - used to create a giant table of results from the mixed effects models
 # example_simulations.Rmd - used to create plots of examples from simulations (e.g. Figures 1, S1 and S2)
-# analysis_simulations.Rmd - used to visualize and calculate summary statistics from the simulations (e.g., population size, phenotype-environment associations)
