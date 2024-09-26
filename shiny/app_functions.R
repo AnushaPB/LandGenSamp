8d91c13031665bd6b0a220573dc16ef55ea69a84
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-09-26 14:56:55 -0700

    Delete shiny directory

--- shiny/app_functions.R
+++
@@ -1,6 +0,0 @@
-get_app_data <- function(method, sampling) {
-  df <- read_csv(here("shiny", "app_data", paste0(method, "_", sampling, ".csv")))
-  # convert parameters to factors
-  df <- var_to_fact(df) 
-  return(df)
-}r
