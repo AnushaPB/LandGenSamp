2dc5f5f841dba4081053ee8388e5cfdc7ed01af6
Anusha Bishop <anusha.bishop@gmail.com>
2024-10-29 12:01:13 -0700

    update outputs

--- p3_methods/p3_methods_job.sh
+++ p3_methods/p3_methods_job.sh
@@ -11,8 +11,16 @@ Rscript simulation_stats.R
 # Run landscape genomic methods
 Rscript run_methods.R
 
+# Move outputs from full GDM runs to a seperate folder
+mkdir -p outputs/fullGDM
+mv outputs/*fullGDM.csv outputs/fullGDM
+
 # Compress outputs for git
 zip -r outputs.zip outputs
-# to decompress: unzip outputs
-
+# split the zip file into 95MB parts for GitHub
+split -b 95m outputs.zip outputs.zip.part_
+rm outputs.zip
+# to decompress: unzip outputs using the following code
+# cat outputs.zip.part_* > outputs.zip
+# unzip outputs.zip
 cd ..
